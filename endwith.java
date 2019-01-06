3407b9a0e9f6b454b1ae76580a9773838f7d3e55
Huan Chieh, Liao <u10600086@lssh.tp.edu.tw>
2019-01-06 22:26:25 +0800

    20190106 Backup

---
+++ endwith.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String a="Hello World";
+        if(a.endsWith("rld")){
+            a=a.replace("Hello ","" );
+                  
+            System.out.print(a);
+        }
+        // TODO code application logic here
+    }
+    
+}
---
+++ replace.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.*;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+        
+    public static void main(String[] args) {
+        String b = "LSSC is Great";
+        b=b.replace("s G","s Super G");        
+        //Scanner scanner=new Scanner(System.
+        //String a = scanner.nextLine();
+        System.out.print(b);
+        // TODO code application logic here
+    }
+    
+}
